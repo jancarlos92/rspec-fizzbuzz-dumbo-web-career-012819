433bc7c41cfe340d783a70331f51cd2062b43f7f
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-07 21:38:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 
 def fizzbuzz(x)
-  if x % 3 == 0 
-    "Fizz" 
+  if x % 15 == 0 
+    "FizzBuzz"  
     elsif x % 5 == 0 
     "Buzz"
-    elsif x / 5 == 3
-    "FizzBuzz"
+    elsif x % 3 == 0
+    "Fizz"
     elsif x / 4 == 0 
     return NIL
  end
